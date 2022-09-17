eb71601a63f573e8ab45fcced2acf00c43f6034e
Anatolii <101575015+Nomzegh@users.noreply.github.com>
2022-09-17 14:49:37 +0200

    Create generate_bytes.py
    Bytes to paste in Commit & Register

---
+++ generate_bytes.py
@@ -0,0 +1,11 @@
+from web3 import Web3, HTTPProvider
+import abi
+
+HTTP_URL = 'url'
+web3 = Web3(HTTPProvider(HTTP_URL))
+print('Web3 connected:', web3.isConnected())
+router = web3.eth.contract(address='0x6D910eDFED06d7FA12Df252693622920fEf7eaA6', abi=contract_abi)
+
+def makeComm(nametag, acc_address, random_secret):
+    x = router.functions.makeCommitment(nametag, acc_address, random_secret).call()
+    print(x)
